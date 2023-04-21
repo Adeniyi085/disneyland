a5a473a0514a1a2390e67658ca60e90b906b1c63
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2023-04-21 23:38:50 +0100

    Cover image added to the Ui

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,7 +1,47 @@
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 
 const Login = (props) => {
-    return <h1>Login</h1>
+    return (
+    <Container>
+        <Content>
+            <BgImage />
+        </Content>
+    </Container>
+    )
 }
+
+const Container = styled.section`
+overflow: hidden;
+display: flex;
+flex-direction: column;
+text-align: center;
+height: 100vh;
+`
+const Content = 
+styled.div`
+margin-bottom: 10vh;
+width: 100%;
+position: relative;
+min-height:100%;
+box-sizing: border-box;
+display:flex;
+justify-content:center;
+align-items: center;
+flex-direction: column;
+padding:80px 40px;
+height: 100%;
+`
+const BgImage = styled.div`
+background-image: url("/images/login-background.jpg");
+height: 100%;
+background-size: cover;
+background-repeat: no-repeat;
+position: absolute;
+top: 0;
+right:0;
+left:0;
+z-index: -1
+`
+
 export default Login
