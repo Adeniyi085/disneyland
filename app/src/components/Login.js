1e610ef4f4ac729bf6089cbecf37a8d02f8a4d3c
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2023-04-23 15:42:06 +0100

    Sytling the Call to Action(CTA) in Login.JS component:

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -5,6 +5,15 @@ const Login = (props) => {
     return (
     <Container>
         <Content>
+            <CTA>
+                <CTALogoOne src= "/images/cta-logo-one.svg" alt=""/>
+                `````  {/* <SignUp>GET ALL THERE</SignUp>
+                    <Description>
+                    Get Premier Access to Raya and the Last Dragon for an additional fee
+                with a Disney+ subscription. As of 03/26/21, the price of Disney+
+                and The Disney Bundle will increase by $1. 
+                    </Description>````` */}
+            </CTA>
             <BgImage />
         </Content>
     </Container>
@@ -43,5 +52,28 @@ right:0;
 left:0;
 z-index: -1
 `
+const CTA = styled.div`
+margin-bottom: 2vh;
+max-width: 650px;
+flex-wrap: wrap;
+display: flex;
+flex-direction: column;
+justify-content: center;
+margin-top: 0;
+align-items: center;
+text-align: center;
+margin-right: auto;
+margin-left: auto;
+transition-timing-function: ease-out;
+transition: opacity 0.2s;
+width: 100%;
+`
 
+const CTALogoOne = styled.img`
+margin-bottom: 12px;
+max-width: 600px;
+min-height: 1px;
+display: block;
+width: 100%
+`
 export default Login
