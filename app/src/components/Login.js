b5725b276eb87dc855a2bac620fa529a3ab67d40
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2023-04-24 03:18:31 +0100

    Hovering effect done on the SignUp button

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -7,12 +7,12 @@ const Login = (props) => {
         <Content>
             <CTA>
                 <CTALogoOne src= "/images/cta-logo-one.svg" alt=""/>
-                `````  {/* <SignUp>GET ALL THERE</SignUp>
-                    <Description>
+                    <SignUp>GET ALL THERE</SignUp>
+                    {/* <Description>
                     Get Premier Access to Raya and the Last Dragon for an additional fee
                 with a Disney+ subscription. As of 03/26/21, the price of Disney+
                 and The Disney Bundle will increase by $1. 
-                    </Description>````` */}
+                    </Description> */}
             </CTA>
             <BgImage />
         </Content>
@@ -68,12 +68,27 @@ transition-timing-function: ease-out;
 transition: opacity 0.2s;
 width: 100%;
 `
-
 const CTALogoOne = styled.img`
 margin-bottom: 12px;
 max-width: 600px;
 min-height: 1px;
 display: block;
 width: 100%
+`
+const SignUp =styled.a`
+font-weight: bold;
+color: #F9F9F9;
+background-color:#0063e5;
+margin-bottom: 12px;
+width: 100%;
+letter-spacing: 1.5px;
+font-size: 18px;
+padding: 16.5px 0;
+border: 1px solid transparent;
+border-radius: 4px;
+ 
+&:hover {
+    background-color: #0483ee;
+}
 `
 export default Login
